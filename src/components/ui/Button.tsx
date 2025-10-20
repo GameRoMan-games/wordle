391fadc5016e4d07878a4073cacd509db7aa54cd
Roman <ranishchenko25@students.psc.ac.uk>
2025-10-20 10:56:51 +0100

    fix

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,9 +10,9 @@ const Button: Component<ButtonProps> = (props) => {
   return (
     <button
       {...rest}
-      class="mt-5 py-2.5 px-5 text-lg leading-none text-(--text-color-light) dark:text-(--text-color-dark)
+      class={`mt-5 py-2.5 px-5 text-lg leading-none text-(--text-color-light) dark:text-(--text-color-dark)
       border-2 border-solid border-(--border-color-light) dark:border-(--border-color-dark) cursor-pointer font-bold uppercase
-      bg-(--content-bg-light) dark:bg-(--content-bg-dark) hover:bg-[#bbb] dark:hover:bg-[#4a4a4a] transition-colors duration-300"
+      bg-(--content-bg-light) dark:bg-(--content-bg-dark) hover:bg-[#bbb] dark:hover:bg-[#4a4a4a] transition-colors duration-300`}
       textContent={label}
     />
   );
